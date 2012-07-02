5a435bfbc08360a638cc9d56ab6ed0b828974e70
Arthur Reutenauer <arthur.reutenauer@normalesup.org>
2012-07-02 21:53:50 +0100

    Major overhaul of the server; try and handle all cases correctly.

--- app/server.rb
+++ app/server.rb
@@ -8,56 +8,99 @@ require 'image_handler'
 
 module FTServer
   class Server
-    def call(env)
-      datadir = File.join(File.dirname(File.realpath(__FILE__)), '..', 'data') # TODO configure
+    # The repository for our scaled pictures is under <root>/data
+    @@datadir = File.join(File.dirname(File.realpath(__FILE__)), '..', 'data')
 
-      req = Rack::Request.new(env)
+    # We support exactly two types of paths:
+    # 1. The root path with a mandatory parameter ‘flickr_id’ and an
+    #    optional ‘text’ (i. e. “/?flickr_id=123def&text=some%20caption”
+    # 2. Paths such as  “/images/abc456.jpg” that serves a previously
+    #    processed image.
+    #
+    # Type 1 paths are handled by the method “transform_image,” type 2 by
+    # “serve_image.”
 
-      if env['REQUEST_METHOD'] == 'GET' # We support only that method.
-        request_path = env['PATH_INFO'] # TODO Why doesn’t REQUEST_PATH work?
-        if request_path =~ /\A\/images\/(.*)\z/
-          response = Rack::Response.new([], 200, { 'Content-Type' => 'image/jpeg' })
+    # 404 in Roman numerals.
+    def cdiv
+      response = Rack::Response.new([], 404).close
+    end
 
-          image_filename = $1
-          filepath = File.join(datadir, image_filename)
-          image_file = File.open(filepath, 'r')
-          response.write(image_file.read(File.size(filepath)))
-          image_file.close
+    def transform_image(env)
+      request = Rack::Request.new(env)
+      response = Rack::Response.new([], 200, { 'Content-Type' => 'text/html' })
 
-          response.close
-        else
-          response = Rack::Response.new([], 200, { 'Content-Type' => 'text/html' })
+      id = request.GET['flickr_id']
+      text = request.GET['text']
 
-          id = req.GET['flickr_id']
-          text = req.GET['text']
+      # user_message is the user-visible message we’ll display on the
+      # HTML page we return.
+      # We have at least three cases: the request contained a valid id;
+      # an invalid id; no id at all.  The optional text is treated as
+      # a subcase of the first one.
 
-          if id
-            value = "Flickr ID #{req.GET['flickr_id']}"
-            if text
-              value = value + " with overlaid text “#{}”"
-            end
-          else
-            value = "Please include a Flickr photo ID in your GET request"
+      if id # We have an id, the real wor begins.
+        begin
+          user_message = "Flickr ID #{request.GET['flickr_id']}"
+          if text && text.length > 0 # We treat empty strings the same way as nils.
+            user_message + " with overlaid text “#{request.GET['text']}”"
           end
 
           handler = ImageHandler.new
-          handler.retrieve(id)
+          handler.retrieve(id) # May raise an exception
           handler.transform(text)
-          new_id = Time.now.to_i.to_s
-          new_filename = new_id + ".jpg"
-          FileUtils.cp(File.join(handler.tempdir.path, 'flickr_image_new.jpg'), File.join(datadir, new_filename))
+          new_id = id.to_s + '_' + Time.now.to_i.to_s
+          new_filename = new_id + '_' + rand(1048576).to_s + '.jpg'
+          FileUtils.cp(File.join(handler.tempdir.path, 'flickr_image_new.jpg'), File.join(@@datadir, new_filename))
           # handler.tempdir.delete # Not yet.
 
-          value += "<p>http://#{env['SERVER_NAME']}/images/#{new_filename}<p>"
+          user_message += "<p>http://#{env['SERVER_NAME']}/images/#{new_filename}<p>"
+        rescue NoSuchFlickrPhotoID => err
+          user_message = err.message
+        end
+      else # if id
+        user_message = "Please include a Flickr photo ID in your GET request"
+      end
 
-          response.write('<title>Flickr Scaler Server</title>')
-          response.write("<p>#{value}</p>")
+      response.write('<title>Flickr Scaler Server</title>')
+      response.write("<p>#{user_message}</p>")
+      response.close
 
-          response.close
+      response
+    end
+
+    def serve_image(image_filename)
+      begin
+        response = Rack::Response.new([], 200, { 'Content-Type' => 'image/jpeg' })
+
+        filepath = File.join(@@datadir, image_filename)
+        image_file = File.open(filepath, 'r') # May raise exception
+        response.write(image_file.read(File.size(filepath)))
+        image_file.close
+        response.close
+      rescue Errno::ENOENT
+        response = cdiv # 404
+      end
+
+      response
+    end
+
+    def call(env)
+      # We support only GET requests for two very particular cases.
+      # For all the rest, we return a 404 code.
+      # That routing method is admittedly simplistic, but it does what
+      # we need.
+      if env['REQUEST_METHOD'] == 'GET' 
+        request_path = env['PATH_INFO']
+        if request_path =~ /\A?/
+          response = transform_image(env)
+        elsif request_path =~ /\A\/images\/(.*)\z/
+          response = serve_image($1)
         end
       end
 
-      response.finish if response
+      # If the request is anything else than the above, return a 404.
+      response ||= Rack::Response.new([], 404)
+      response.finish
     end
   end
 end
